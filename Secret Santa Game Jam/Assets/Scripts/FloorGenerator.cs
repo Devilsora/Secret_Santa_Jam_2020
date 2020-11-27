202c98b750b3da41cee2ef76314bb797b9798ea2
Joshua Marlowe <joshben@cox.net>
2020-11-27 03:45:43 -0800

    Preliminary Level Generation
    Added some kenny assets to use, world generation still has alot of work needed to get more varied rooms and less hallways

---
+++ Secret Santa Game Jam/Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour
+{
+    public RoomData roomPrefab;
+
+    public Transform floorParent;
+
+    public List<GameObject> basicEnemies;
+    public List<GameObject> bosses;
+    public List<GameObject> treasures;
+    public List<GameObject> decorations;
+
+    public float roomWidth = 40f;
+    public float roomLength = 40f;
+
+    [SerializeField]
+    [Range(10, 30)] 
+    private int roomsToSpawn;
+
+    private int roomsSpawned;
+    public bool finishedSpawning;
+
+    Direction opposite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start by creating a room with no walls - should kick off whole room creation process
+        GameObject entryRoom = Instantiate(roomPrefab.gameObject, floorParent);
+        entryRoom.name = "Entry Room";
+        RoomData entryData = entryRoom.GetComponent<RoomData>();
+
+        entryData.GetGenerationData(this);
+
+        //four different coroutines to handle for each direction vs. having the 1 for loop
+        
+        Debug.Log("Starting generation going north");
+        entryRoom.GetComponent<RoomData>().RemoveWall(Direction_Val.NORTH);
+        StartCoroutine(GenerateRoom(Direction_Val.NORTH, entryData));
+
+        Debug.Log("Starting generation going south");
+        entryRoom.GetComponent<RoomData>().RemoveWall(Direction_Val.SOUTH);
+        StartCoroutine(GenerateRoom(Direction_Val.SOUTH, entryData));
+
+        Debug.Log("Starting generation going east");
+        entryRoom.GetComponent<RoomData>().RemoveWall(Direction_Val.EAST);
+        StartCoroutine(GenerateRoom(Direction_Val.EAST, entryData));
+
+        Debug.Log("Starting generation going west");
+        entryRoom.GetComponent<RoomData>().RemoveWall(Direction_Val.WEST);
+        StartCoroutine(GenerateRoom(Direction_Val.WEST, entryData));
+
+        //for (int i = 0; i < (int) Direction_Val.MAXDIRECTIONS; i++)
+        //{
+        //    entryRoom.GetComponent<RoomData>().RemoveWall((Direction_Val)i);
+        //    Debug.Log("Spawning rooms in start method");
+        //    
+        //    StartCoroutine(GenerateRoom((Direction_Val) i, entryData));
+        //}
+
+        //spawn rooms until we hit the spawn limit
+        //have rooms start with no exits/entrances, then decide which ones to open up based on where they're connected to
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerateRoom(Direction_Val d, RoomData prev)
+    {
+        
+        if (finishedSpawning)
+        {
+            yield break;
+        }
+
+        
+        List<Direction_Val> openDirections = new List<Direction_Val>();
+
+        GameObject newRoom = Instantiate(roomPrefab.gameObject, floorParent);
+        RoomData newRoomData = newRoom.GetComponent<RoomData>();
+
+        newRoom.name = "Room " + roomsSpawned;
+
+        newRoomData.GetGenerationData(this);
+
+        Vector3 newRoomPos = prev.gameObject.transform.position;
+
+        switch (d)
+        {
+            case Direction_Val.NORTH:
+                newRoomPos.z += roomLength;
+                break;
+
+            case Direction_Val.SOUTH:
+                newRoomPos.z -= roomLength;
+                break;
+
+            case Direction_Val.EAST:
+                newRoomPos.x += roomWidth;
+                break;
+
+            case Direction_Val.WEST:
+                newRoomPos.x -= roomWidth;
+                break;
+        }
+        
+        newRoom.transform.position = newRoomPos;
+
+        newRoomData.SetNeighbor(opposite.GetOpposite(d), prev);
+        prev.SetNeighbor(d, newRoomData);
+
+        for (int i = 0; i < (int)Direction_Val.MAXDIRECTIONS; i++)
+        {
+            if(!newRoomData.IsNeighborOccupied((Direction_Val)i))
+                openDirections.Add((Direction_Val)i);
+        }
+
+        Debug.Log("Num possible directions: " + openDirections.Count);
+
+        //randomize number of new directions to branch off from 
+        int branchingRooms = Random.Range(1, 3);    //make this greater than 0, and fill in walls later
+
+        for (int i = 0; i < branchingRooms; i++)
+        {
+            //choose a direction
+            Direction_Val randDirection = openDirections[Random.Range(0, openDirections.Count - 1)];
+            openDirections.Remove(randDirection);
+            Debug.Log("Making room in random direction of " + randDirection);
+            StartCoroutine(GenerateRoom(randDirection, newRoomData));
+        }
+
+    }
+
+    public void IncrementRoomCounter()
+    {
+        roomsSpawned++;
+        if (roomsSpawned >= roomsToSpawn)
+            finishedSpawning = true;
+    }
+
+    public int GetRoomsSpawned()
+    {
+        return roomsSpawned;
+    }
+
+}
---
+++ Secret Santa Game Jam/Assets/Scripts/RoomData.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+
+public enum Direction_Val
+{
+    INVALID = -1,
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    MAXDIRECTIONS
+
+}
+
+public struct Direction
+{
+    public Direction_Val GetOpposite(Direction_Val d)
+    {
+        switch (d)
+        {
+            case Direction_Val.NORTH:
+                return Direction_Val.SOUTH;
+
+            case Direction_Val.EAST:
+                return Direction_Val.WEST;
+
+            case Direction_Val.SOUTH:
+                return Direction_Val.NORTH;
+
+            case Direction_Val.WEST:
+                return Direction_Val.EAST;
+        }
+
+        return Direction_Val.INVALID;
+    }
+}
+
+public class RoomData : MonoBehaviour
+{
+    public GameObject[] Walls;
+    public Transform RoomDecorationTransform;
+
+    public float decoration_min_X = -10f;
+    public float decoration_max_X = 17f;
+    public float decoration_min_Z = -15f;
+    public float decoration_max_Z = 12f;
+
+    [Range(-10f, 17f)] private int decorationX;
+    [Range(-15f, 12f)] private int decorationZ;
+
+    public FloorGenerator floorGen;
+
+    public List<Direction_Val> openDirections;
+
+    public List<RoomData> connectedRooms = new List<RoomData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        
+    }
+
+    public GameObject[] getWalls()
+    {
+        return Walls;
+    }
+
+    public void GetGenerationData(FloorGenerator gen)
+    {
+        floorGen = gen;
+        floorGen.IncrementRoomCounter();
+
+        //when assigning, check what directions are open, add to array
+        for (int i = 0; i < (int) Direction_Val.MAXDIRECTIONS; i++)
+        {
+            if(!Walls[i].activeInHierarchy)
+                openDirections.Add((Direction_Val)i);
+        }
+
+        for (int i = 0; i < (int)Direction_Val.MAXDIRECTIONS; i++)
+        {
+            connectedRooms.Insert(i, null);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Room")
+            Destroy(gameObject);
+    }
+
+    //sets the neighbors of each, opens the wall values of each as well
+    //returns true if it can link up the rooms together, returns false if it can't
+    public bool SetNeighbor(Direction_Val d, RoomData neighbor)
+    {
+        //check if both the potential neighbor and this room can link up
+        Direction dir;
+        Direction_Val op = dir.GetOpposite(d);
+
+        Debug.Log("Connected rooms: " + connectedRooms.Count);
+        //check for this room in that direction and the neighbor room in the opposite direction
+        if (connectedRooms[(int) d] == null && !neighbor.IsNeighborOccupied(op))
+        {
+            connectedRooms[(int)d] = neighbor;
+            RemoveWall(op);
+            
+            neighbor.connectedRooms[(int) op] = this;
+            neighbor.RemoveWall(d);
+
+            return true;
+        }
+        
+        return false;
+    }
+
+    //returns if room is already linked up at direction value d
+    public bool IsNeighborOccupied(Direction_Val d)
+    {
+        if (connectedRooms[(int) d] != null)
+            return true;
+
+        return false;
+    }
+
+    public void RemoveWall(Direction_Val d)
+    {
+        Debug.Log("removing walls at " + d);
+        Walls[(int)d].SetActive(false);
+    }
+
+    public void RemoveWall(int direction)
+    {
+        Debug.Log("removing walls at " + direction);
+        Walls[direction].SetActive(false);
+    }
+
+    public void AddWall(Direction_Val d)
+    {
+        Walls[(int)d].SetActive(true);
+    }
+
+    public bool AddDecoration(Vector3 pos, GameObject obj)
+    {
+        //check if position is valid
+        if ((pos.x >= Mathf.Min(decoration_min_X, decoration_max_X) && pos.x <= Mathf.Max(decoration_min_X, decoration_max_X)) &&
+            (pos.z >= Mathf.Min(decoration_min_Z, decoration_max_Z) && pos.z <= Mathf.Max(decoration_min_Z, decoration_max_Z)))
+        {
+            obj.transform.SetParent(RoomDecorationTransform);
+            obj.transform.position = pos;
+
+            return true;
+        }
+
+
+        return true;
+    }
+}
